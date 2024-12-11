af1ad692ab318a1c7d5e50f635b2365d73645d80
Erik Berger <berger.erik12@gmail.com>
2024-12-11 16:02:31 +0100

    git init wordpress eCommerce-Shop

---
+++ wp-content/plugins/klarna-payments-for-woocommerce/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '3.8.3',
+        'version' => '3.8.3.0',
+        'reference' => '24c3a997ca6892344a517149d210f5a24b62cdd1',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '3.8.3',
+            'version' => '3.8.3.0',
+            'reference' => '24c3a997ca6892344a517149d210f5a24b62cdd1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
